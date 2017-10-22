8b91da86a1111bc459636c02d76700c6a8650e1a
Nguyen Le <trieunguyen30@gmail.com>
2017-10-22 21:00:07 +0700

    commit1

---
+++ Bai3-Server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai3_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            private static DHCPServer dhcpd;
+
+        static void Main(string[] args)
+        {
+            dhcpd = new DHCPServer(100, 150);
+            while (true)
+                dhcpd.startListening();   
+        }
+    }
+}
